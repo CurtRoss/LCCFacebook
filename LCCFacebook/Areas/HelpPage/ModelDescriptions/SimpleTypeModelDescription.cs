abb7f8dd62f1abd78ea084ea6386f370e0e88cc5
Curt Reigelsperger <curt.reigelsperger@gmail.com>
2021-02-02 21:33:53 -0500

    initial commit

---
+++ LCCFacebook/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LCCFacebook.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ LCCFacebook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LCCFacebook.WebApiApplication" Language="C#" %>
