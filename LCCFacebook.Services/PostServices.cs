edefba1a629f4b424ea3a9d99d633561dd260093
Curt Reigelsperger <curt.reigelsperger@gmail.com>
2021-02-04 13:32:11 -0500

    updated Post sections

--- LCCFacebook.Services/PostServices.cs
+++ LCCFacebook.Services/PostServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LCCFacebook.Data;
+using LCCFacebook.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,51 @@ namespace LCCFacebook.Services
 {
     public class PostServices
     {
+        private readonly Guid _userId;
+
+        public PostServices(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreatePost(PostCreate model)
+        {
+            var entity =
+                new Post()
+                {
+                    OwnerId = _userId,
+                    UserName = System.Security.Principal.WindowsIdentity.GetCurrent().Name,
+                    Content = model.Content,
+                    CreateUtc = DateTimeOffset.Now
+
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<PostListItem> GetPosts()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Posts
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new PostListItem
+                                {
+                                    PostId = e.PostId,
+                                    UserName = e.UserName,
+                                    CreatedUtc = e.CreateUtc,
+                                    ModifiedUtc = e.ModifiedUtc,
+                                    Content = e.Content
+                                }
+                    );
+            }
+        }
     }
 }
